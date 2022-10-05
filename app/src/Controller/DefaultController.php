dc8adef0a31fc454da5440185dabef30097d49a4
Lukas Kondrotas <kondrotaslukas@gmail.com>
2022-10-05 16:18:32 +0300

    HTBE: expanded controller and twig file

--- app/src/Controller/DefaultController.php
+++ app/src/Controller/DefaultController.php
@@ -5,13 +5,21 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends AbstractController
 {
-    public function index()
+    public function index(): Response
     {
         return $this->render(
             '/templates/base.html.twig', []
+        );
+    }
+
+    public function login(): Response
+    {
+        return $this->render(
+            '/templates/login.html.twig', []
         );
     }
 }
